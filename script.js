52aa0d9aee6671a1968516fe24401afbf201c34d
silvataislaine <128072139+silvataislaine@users.noreply.github.com>
2024-07-31 13:23:41 -0300

    tais

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-
 const perguntas = [
     {
         enunciado: "Você acha agricultura importante para a economia?",
